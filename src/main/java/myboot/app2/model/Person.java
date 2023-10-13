94f8601cab408c1a4de317c38f6319bb29a8a021
Achraf <achraf.ZERHOUNI-ABDOU@etu.univ-amu.fr>
2023-10-13 13:47:24 +0200

    Person Update (@NotNull)

--- src/main/java/myboot/app2/model/Person.java
+++ src/main/java/myboot/app2/model/Person.java
@@ -4,6 +4,8 @@ package myboot.app2.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
@@ -13,11 +15,20 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
     private String firstName;
+
+    @NotEmpty
     private String lastName;
+
+    @NotEmpty
     private String email;
     private String website;
+
+    @NotNull
     private Date birthDate;
+
+    @NotEmpty
     private String password;
 
     @OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
