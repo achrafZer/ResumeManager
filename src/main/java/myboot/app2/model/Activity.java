2a7095fff1b9427c4cf55247cb7351d0e4605ecf
Achraf <achraf.ZERHOUNI-ABDOU@etu.univ-amu.fr>
2023-10-13 13:27:36 +0200

    Activity Update (@NotNull)

--- src/main/java/myboot/app2/model/Activity.java
+++ src/main/java/myboot/app2/model/Activity.java
@@ -2,11 +2,9 @@ package myboot.app2.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -16,10 +14,20 @@ public class Activity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Column(nullable = false)
     private int startYear;
+
+    @NotNull
+    @Column(nullable = false)
     private int endYear;
 
-    private ActivityNature nature; //Expérience pro / Formation...
+    @NotNull
+    @Column(nullable = false)
+    private ActivityNature nature; //Ex : Expérience pro / Formation...
+
+    @NotNull
+    @Column(nullable = false)
     private String title; //Ex : Chef de projets informatique / Licence / Master...
 
     @Size(max = 100000)
