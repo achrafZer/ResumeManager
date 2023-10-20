c60698c69fdbb740ebac3c27780cc65cce954637
Achraf <zerhouniachraf@hotmail.com>
2023-10-20 23:44:55 +0200

    JavaDoc on model

--- src/main/java/myboot/app/security/AuthentificationService.java
+++ src/main/java/myboot/app/security/AuthentificationService.java
@@ -1,2 +1,30 @@
-package myboot.app.service;public class AuthentificationService {
+package myboot.app.security;
+
+import myboot.app.model.Person;
+import myboot.app.dao.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service class for managing authentication operations.
+ */
+@Service
+public class AuthentificationService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
 }
