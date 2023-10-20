f4a2610d75f796b24f2f66b587adf3552f1d460c
Achraf <zerhouniachraf@hotmail.com>
2023-10-20 23:38:13 +0200

    Javadoc ActivityNature.java

--- src/main/java/myboot/app/model/ActivityNature.java
+++ src/main/java/myboot/app/model/ActivityNature.java
@@ -1,8 +1,30 @@
 package myboot.app.model;
 
+/**
+ * Enumerates the possible natures or types of activities that can be associated with a CV.
+ *
+ * @author Achraf
+ * @version 1.0
+ */
 public enum ActivityNature {
+
+    /**
+     * Represents a professional work experience.
+     */
     PROFESSIONAL_EXPERIENCE,
+
+    /**
+     * Represents formal educational experiences such as schooling or training.
+     */
     EDUCATION,
+
+    /**
+     * Represents leisure or personal activities not directly related to work or education.
+     */
     LEISURE,
+
+    /**
+     * Represents projects, which can be of various kinds, including academic, personal, or professional.
+     */
     PROJECT
 }
