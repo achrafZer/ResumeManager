fea3a4338c883e345e117e0f5d1426a2ac3fbbe6
Achraf <zerhouniachraf@hotmail.com>
2023-10-20 22:39:21 +0200

    JwtProvider created and tested

--- src/main/java/myboot/app/security/JwtProvider.java
+++ src/main/java/myboot/app/security/JwtProvider.java
@@ -1,2 +1,82 @@
-package myboot.app.security;public class JwtProvider {
+package myboot.app.security;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import myboot.app.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+
+import io.jsonwebtoken.Claims;
+
+
+/**
+ * Provides methods to create and validate JWT.
+ */
+@Component
+public class JwtProvider {
+    @Value("${security.jwt.token.secret-key:secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds = 3600000; // 1 hour
+
+    /**
+     * Initializes the secret key after bean creation.
+     */
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    /**
+     * Creates a JWT token based on the provided person.
+     *
+     * @param person the person for whom the token is created
+     * @return the generated JWT token
+     */
+    public String createToken(Person person) {
+        Claims claims = Jwts.claims().setSubject(person.getEmail());
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder().setClaims(claims).setIssuedAt(now).setExpiration(validity).signWith(SignatureAlgorithm.HS256, secretKey).compact();
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+        this.init();
+    }
+
+    /**
+     * Validates the provided token.
+     *
+     * @param token the JWT token
+     * @return true if token is valid, false otherwise
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Extracts the username (in our case, email) from the provided token.
+     *
+     * @param token the JWT token
+     * @return the extracted username (email)
+     */
+    public String getUsername(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
 }
---
+++ src/test/java/myboot/app/security/TestJwtProvider.java
@@ -0,0 +1,56 @@
+package myboot.app.security;
+
+import myboot.app.model.Person;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "security.jwt.token.secret-key=thisIsASuperSecretKeyForJwtTestsYesItIsLong!",
+        "security.jwt.token.expire-length=3600000"
+})
+public class TestJwtProvider {
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    private Person testPerson;
+
+    @BeforeEach
+    public void setUp() {
+        testPerson = new Person();
+        testPerson.setEmail("test@email.com");
+        testPerson.setPassword("myPassword");
+        testPerson.setFirstName("PersonJWT");
+        testPerson.setLastName("PersonLastJWT");
+        // Populate other fields if required
+    }
+
+    @Test
+    public void whenGenerateJwt_thenSuccess() {
+        String token = jwtProvider.createToken(testPerson);
+        assertNotNull(token);
+        assertTrue(jwtProvider.validateToken(token));
+    }
+
+    @Test
+    public void whenGetUsernameFromJwt_thenCorrect() {
+        String token = jwtProvider.createToken(testPerson);
+        String username = jwtProvider.getUsername(token);
+        assertEquals(testPerson.getEmail(), username);
+    }
+
+    @Test
+    public void whenValidateJwtWithWrongKey_thenFail() {
+        String token = jwtProvider.createToken(testPerson);
+        JwtProvider wrongKeyProvider = new JwtProvider();
+        wrongKeyProvider.setSecretKey("thisIsTotalyWrongIJustAmCreatingATooLongKeyForThisTest");
+        wrongKeyProvider.init();
+        assertFalse(wrongKeyProvider.validateToken(token));
+    }
+}
