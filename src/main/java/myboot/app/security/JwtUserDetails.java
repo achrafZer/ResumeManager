0ba6af89a12d3eb188d93bbf07e4655e71534b22
Achraf <zerhouniachraf@hotmail.com>
2023-10-21 15:53:37 +0200

    SpringSecurityOpen deleted, Creating JwtUserDetails

---
+++ src/main/java/myboot/app/security/JwtUserDetails.java
@@ -0,0 +1,10 @@
+package myboot.app.security;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Provides implementation for loading user-specific data during authentication.
+ */
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+}
