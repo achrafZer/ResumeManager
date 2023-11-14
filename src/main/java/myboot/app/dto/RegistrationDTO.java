c87c7076c5c461680f594b16048d83fe96310189
Achraf ZERHOUNI <zerhouniachraf@hotmail.com>
2023-11-14 15:43:40 +0100

    registration information as attrubuts in the class

--- src/main/java/myboot/app/dto/RegistrationDTO.java
+++ src/main/java/myboot/app/dto/RegistrationDTO.java
@@ -1,4 +1,22 @@
 package myboot.app.dto;
 
+import java.awt.*;
+import java.util.Date;
+import java.util.Set;
+
 public class RegistrationDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String website;
+
+    private Date birthDate;
+
+    private String password;
+
+    Set<String> roles;
 }
