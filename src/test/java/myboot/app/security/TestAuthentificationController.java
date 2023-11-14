2255ad5223c6fe4fb355032dd77ee3716294c3a9
Achraf ZERHOUNI <zerhouniachraf@hotmail.com>
2023-11-14 21:12:00 +0100

    test : Tested the /secu-users/signup endpoint

--- src/test/java/myboot/app/security/TestAuthentificationController.java
+++ src/test/java/myboot/app/security/TestAuthentificationController.java
@@ -1,67 +1,103 @@
 package myboot.app.security;
 
 import myboot.app.dao.PersonRepository;
+import myboot.app.dao.XUserRepository;
 import myboot.app.dto.RegistrationDTO;
+import myboot.app.model.Person;
 import myboot.app.model.XUser;
-import myboot.app5.security.UserService;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.text.ParseException;
+import javax.persistence.EntityManager;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 public class TestAuthentificationController {
-
-    @Autowired
-    PersonRepository personRepository;
 
     @LocalServerPort
     private int port;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private TestRestTemplate restTemplate;
 
-    private String baseUrl;
+    @Autowired
+    private XUserRepository userRepository;
 
-    @BeforeEach
-    public void setUp() {
-        baseUrl = "http://localhost:" + port + "/secu-users";
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    private XUser user;
+
+    private Person person;
+
+    @AfterEach
+    public void cleanup() {
+        userRepository.deleteById(user.getUserName());
+        personRepository.delete(person);
     }
 
-    @Test
-    public void testEmailSignup() throws ParseException {
-        System.out.println(personRepository.findAll());
 
+
+    @Test
+    public void testSignup() throws Exception {
+        String baseUrl = "http://localhost:" + port + "/secu-users/signup";
         RegistrationDTO registrationDTO = new RegistrationDTO();
-        registrationDTO.setFirstName("Achraf");
-        registrationDTO.setLastName("ZERHOUNI");
-        registrationDTO.setEmail("zerhouniachraf@hotmail.com");
+        registrationDTO.setEmail("test@example.com");
+        registrationDTO.setPassword("password");
+        registrationDTO.setFirstName("Test");
+        registrationDTO.setLastName("User");
+
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date birthday = sdf.parse("1990-01-01");
         registrationDTO.setBirthDate(birthday);
-        registrationDTO.setPassword("mon MDP");
-        Set<String> roles = new HashSet<>();
-        roles.add("USER");
-        registrationDTO.setRoles(roles);
 
-        UserService userService = new UserService();
-        userService.signup(registrationDTO);
+        registrationDTO.setRoles(new HashSet<>(Collections.singletonList("USER")));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<RegistrationDTO> request = new HttpEntity<>(registrationDTO, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(baseUrl, request, String.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        assertEquals("User registered successfully", response.getBody());
+
+
+        user = userRepository.findById("test@example.com").orElse(null);
+        assertNotNull(user);
+        assertTrue(entityManager.contains(user));
+        assertEquals("test@example.com", user.getUserName());
+        assertTrue(passwordEncoder.matches("password", user.getPassword()));
+
+        person = personRepository.findByEmail("test@example.com");
+        assertNotNull(person);
+        assertTrue(entityManager.contains(person));
+        assertEquals("Test", person.getFirstName());
+        assertEquals("User", person.getLastName());
+        assertEquals("test@example.com", person.getEmail());
+        assertTrue(passwordEncoder.matches("password", person.getPassword()));
 
     }
-
 }
