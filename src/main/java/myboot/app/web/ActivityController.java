23789b2fa06c01e9b1a2abd9b239d6d98799fc6b
Achraf ZERHOUNI <zerhouniachraf@hotmail.com>
2023-11-24 21:35:22 +0100

    doc>swagger : activity controller DONE

--- src/main/java/myboot/app/web/ActivityController.java
+++ src/main/java/myboot/app/web/ActivityController.java
@@ -1,5 +1,8 @@
 package myboot.app.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import myboot.app.dao.ActivityRepository;
 import myboot.app.model.Activity;
 import myboot.app.model.Person;
@@ -23,36 +26,54 @@ public class ActivityController {
 
 
     @PostMapping
+    @Operation(summary = "Créer une nouvelle activité")
+    @ApiResponse(responseCode = "201", description = "Activité créée avec succès")
     public ResponseEntity<Activity> createActivity(@Valid @RequestBody Activity activity) {
         Activity savedActivity = activityService.saveActivity(activity);
         return new ResponseEntity<>(savedActivity, HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Récupérer toutes les activités")
+    @ApiResponse(responseCode = "200", description = "Liste des activités récupérée")
     public ResponseEntity<List<Activity>> getAllActivities() {
         List<Activity> activities = activityService.getAllActivities();
         return new ResponseEntity<>(activities, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Récupérer une activité par son ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Activité trouvée"),
+            @ApiResponse(responseCode = "404", description = "Activité non trouvée")
+    })
     public ResponseEntity<Activity> getActivityById(@PathVariable Long id) {
         Optional<Activity> optionalActivity = Optional.ofNullable(activityService.getActivityById(id));
         return optionalActivity.map(activity -> new ResponseEntity<>(activity, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/search-by-activity-title")
+    @Operation(summary = "Rechercher des activités par titre")
+    @ApiResponse(responseCode = "200", description = "Activités correspondant au titre fourni")
     public ResponseEntity<List<Activity>> getActivityByTitle(@RequestParam String activityTitle) {
         List<Activity> activitieList = activityService.getActivityByTitle(activityTitle);
         return new ResponseEntity<>(activitieList, HttpStatus.OK);
     }
 
     @GetMapping("/search-by-person-id")
+    @Operation(summary = "Récupérer des activités par l'ID de la personne dont le CV les contient")
+    @ApiResponse(responseCode = "200", description = "Activités de la personne spécifiée")
     public ResponseEntity<List<Activity>> getActivitiesByPerson(@RequestParam Long id) {
         List<Activity> activitieList = activityService.getActivitiesByPersonId(id);
         return new ResponseEntity<>(activitieList, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Mettre à jour une activité")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Activité mise à jour avec succès"),
+            @ApiResponse(responseCode = "404", description = "Activité non trouvée")
+    })
     public ResponseEntity<Activity> updateActivity(@PathVariable Long id, @Valid @RequestBody Activity updatedActivity) {
         try {
             Activity activity = activityService.updateActivity(id, updatedActivity);
@@ -65,6 +86,11 @@ public class ActivityController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Supprimer une activité")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Activité supprimée avec succès"),
+            @ApiResponse(responseCode = "404", description = "Activité non trouvée")
+    })
     public ResponseEntity<Void> deleteActivity(@PathVariable Long id) {
         if (activityService.existsById(id)) {
             activityService.deleteActivityById(id);
