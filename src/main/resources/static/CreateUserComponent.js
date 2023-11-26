1c2de8424139e0f163f055760128c1ab79950a85
Achraf ZERHOUNI <zerhouniachraf@hotmail.com>
2023-11-26 13:34:00 +0100

    bootstrap for edit-profile page

--- src/main/resources/static/CreateUserComponent.js
+++ src/main/resources/static/CreateUserComponent.js
@@ -1,43 +1,59 @@
 export default {
   template: `
-      <div class="container mt-5">
-        <h2>Créer un Utilisateur</h2>
-        <form @submit.prevent="submitForm">
-          <div class="mb-3">
-            <label for="firstName">Prénom</label>
-            <input type="text" id="firstName" v-model="user.firstName" class="form-control">
-            <span class="text-danger">{{ errors.firstName }}</span>
-          </div>
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+                        <div class="mb-md-5 mt-md-4 pb-5">
+                            <h2 class="fw-bold mb-2 text-uppercase">Créer un Utilisateur</h2>
+                            <p class="text-white-50 mb-5">Veuillez remplir les informations suivantes</p>
 
-          <div class="mb-3">
-            <label for="lastName">Nom</label>
-            <input type="text" id="lastName" v-model="user.lastName" class="form-control">
-            <span class="text-danger">{{ errors.lastName }}</span>
+                            <form @submit.prevent="submitForm">
+                                <div class="form-outline form-white mb-4">
+                                    <label for="firstName" class="form-label">Prénom</label>
+                                    <input type="text" id="firstName" v-model="user.firstName" class="form-control form-control-lg">
+                                    <span class="text-danger">{{ errors.firstName }}</span>
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label for="lastName" class="form-label">Nom</label>
+                                    <input type="text" id="lastName" v-model="user.lastName" class="form-control form-control-lg">
+                                    <span class="text-danger">{{ errors.lastName }}</span>
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label for="email" class="form-label">Adresse Mail</label>
+                                    <input type="email" id="email" v-model="user.email" class="form-control form-control-lg">
+                                    <span class="text-danger">{{ errors.email }}</span>
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label for="password" class="form-label">Mot-de-Passe</label>
+                                    <input type="password" id="password" v-model="user.password" class="form-control form-control-lg">
+                                    <span class="text-danger">{{ errors.password }}</span>
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label for="birthDate" class="form-label">Date de Naissance</label>
+                                    <input type="date" id="birthDate" v-model="user.birthDate" class="form-control form-control-lg">
+                                    <span class="text-danger">{{ errors.birthDate }}</span>
+                                </div>
+
+                                <button type="submit" class="btn btn-outline-light btn-lg px-5">Créer</button>
+                            </form>
+
+                            <p v-if="successMessage" class="text-success">{{ successMessage }}</p>
+                            <p v-if="errorMessage" class="text-danger">{{ errorMessage }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
+</section>
 
-          <div class="mb-3">
-            <label for="email">Adresse Mail</label>
-            <input type="email" id="email" v-model="user.email" class="form-control">
-            <span class="text-danger">{{ errors.email }}</span>
-            </div>
-
-          <div class="mb-3">
-            <label for="password">Mot-de-Passe</label>
-            <input type="password" id="password" v-model="user.password" class="form-control">
-            <span class="text-danger">{{ errors.password }}</span>
-            </div>
-
-          <div class="mb-3">
-            <label for="birthDate">Date de Naissance</label>
-            <input type="date" id="birthDate" v-model="user.birthDate" class="form-control">
-            <span class="text-danger">{{ errors.birthDate }}</span>
-            </div>
-
-          <button type="submit" class="btn btn-primary">Créer</button>
-        </form>
-        <p v-if="successMessage" class="text-success">{{ successMessage }}</p>
-        <p v-if="errorMessage" class="text-danger">{{ errorMessage }}</p>
-      </div>
     `,
   data() {
     return {
