7d1e34954ecb1f3a6531246da8468c25748faada
Achraf ZERHOUNI <zerhouniachraf@hotmail.com>
2023-11-26 13:57:33 +0100

    first version of bootstrap

--- src/main/resources/static/EditActivityComponent.js
+++ src/main/resources/static/EditActivityComponent.js
@@ -2,36 +2,50 @@ import axios from './axios-config.js';
 
 export default {
     template: `
-      <div class="container mt-5">
-        <h2>Modifier l'Activité</h2>
-        <form @submit.prevent="updateActivity">
-          <div class="mb-3">
-            <label for="title">Titre</label>
-            <input type="text" id="title" v-model="activity.title" class="form-control">
-            <span class="text-danger">{{ errors.title }}</span>
-          </div>
-          
-          <div class="mb-3">
-            <label for="description">Description</label>
-            <textarea id="description" v-model="activity.description" class="form-control"></textarea>
-            <span class="text-danger">{{ errors.description }}</span>
-          </div>
-          
-          <div class="mb-3">
-            <label for="startYear">Année de Début</label>
-            <input type="number" id="startYear" v-model="activity.startYear" class="form-control">
-            <span class="text-danger">{{ errors.startYear }}</span>
-          </div>
-          
-          <div class="mb-3">
-            <label for="endYear">Année de Fin</label>
-            <input type="number" id="endYear" v-model="activity.endYear" class="form-control">
-            <span class="text-danger">{{ errors.endYear }}</span>
-          </div>
-          <span class="text-danger">{{ errors.startYearBeforeEndYear }}</span>
-          <button type="submit" class="btn btn-success">Sauvegarder les modifications</button>
-        </form>
-      </div>
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+                        <div class="mb-md-5 mt-md-4 pb-5">
+                            <h2 class="fw-bold mb-4 text-uppercase">Modifier l'Activité</h2>
+                            
+                            <form @submit.prevent="updateActivity">
+                                <div class="form-outline form-white mb-4">
+                                    <label for="title" class="form-label">Titre</label>
+                                    <input type="text" id="title" v-model="activity.title" class="form-control form-control-lg">
+                                    <span class="text-danger">{{ errors.title }}</span>
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label for="description" class="form-label">Description</label>
+                                    <textarea id="description" v-model="activity.description" class="form-control form-control-lg" style="height: 150px;"></textarea>
+                                    <span class="text-danger">{{ errors.description }}</span>
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label for="startYear" class="form-label">Année de Début</label>
+                                    <input type="number" id="startYear" v-model="activity.startYear" class="form-control form-control-lg">
+                                    <span class="text-danger">{{ errors.startYear }}</span>
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label for="endYear" class="form-label">Année de Fin</label>
+                                    <input type="number" id="endYear" v-model="activity.endYear" class="form-control form-control-lg">
+                                    <span class="text-danger">{{ errors.endYear }}</span>
+                                </div>
+
+                                <span class="text-danger">{{ errors.startYearBeforeEndYear }}</span>
+                                <button type="submit" class="btn btn-outline-light btn-lg px-5">Sauvegarder les modifications</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
     `, data() {
         return {
             activity: {
--- src/main/resources/static/resume.js
+++ src/main/resources/static/resume.js
@@ -3,27 +3,43 @@ import axios from './axios-config.js';
 export default {
 
     template: `
-      <div class="container mt-5">
-        <div v-if="person">
-          <h2>CV de {{ person.firstName }} {{ person.lastName }}</h2>
-          <h3>Informations Personnelles</h3>
-          <p>Email: {{ person.email }}</p>
-          <p>Site Web: {{ person.website }}</p>
-          <p>Date de naissance: {{ formatDate(person.birthDate) }}</p>
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-15 col-md-15 col-lg-20 col-xl-12">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+                        <div class="mb-md-5 mt-md-4 pb-5">
+                            <div v-if="person">
+                                <h2 class="fw-bold mb-2 text-uppercase">CV de {{ person.firstName }} {{ person.lastName }}</h2>
+                                <div class="text-start">
+                                    <h3 class="mb-4">Informations Personnelles</h3>
+                                    <p class="text-white-50 mb-2">Email: {{ person.email }}</p>
+                                    <p class="text-white-50 mb-2">Site Web: {{ person.website }}</p>
+                                    <p class="text-white-50 mb-4">Date de naissance: {{ formatDate(person.birthDate) }}</p>
+                                </div>
 
-          <h3>Expériences et Formations</h3>
-          <ul v-if="person.cv && person.cv.activities.length">
-            <li v-for="activity in person.cv.activities" :key="activity.id">
-              <strong>{{ activity.title }}</strong> ({{ activity.startYear }} - {{ activity.endYear }})
-              <p>{{ activity.description }}</p>
-            </li>
-          </ul>
-          <p v-else>Aucune activité de CV disponible.</p>
+                                <div class="text-start">
+                                    <h3>Expériences et Formations</h3>
+                                    <ul v-if="person.cv && person.cv.activities.length" class="list-unstyled">
+                                        <li v-for="activity in person.cv.activities" :key="activity.id">
+                                            <strong>{{ activity.title }}</strong> ({{ activity.startYear }} - {{ activity.endYear }})
+                                            <p>{{ activity.description }}</p>
+                                        </li>
+                                    </ul>
+                                    <p v-else class="text-white-50">Aucune activité de CV disponible.</p>
+                                </div>
+                            </div>
+                            <div v-else>
+                                <p class="text-white-50">Chargement...</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div v-else>
-          <p>Chargement...</p>
-        </div>
-      </div>
+    </div>
+</section>
     `,
 
     data() {
